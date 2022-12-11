78ee14a4be330e07f1ef7cbaf899986a1623a440
heeeydima <aet.mityai@bk.ru>
2022-12-11 15:33:00 +0500

    Future code

--- src/TIMUS/task_1209.java
+++ src/TIMUS/task_1209.java
@@ -1,7 +1,26 @@
 package TIMUS;
 
 import java.io.Console;
+import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class task_1209 {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in );
+        long num = sc.nextLong();
+        StringJoiner result = new StringJoiner(" ");
 
+        while (num > 0) {
+            long x = sc.nextLong();
+            double s = Math.sqrt(8*x-7);
+            if (s % 1 == 0)
+                result.add("1");
+            else
+                result.add("0");
+            num--;
+        }
+        sc.close();
+
+        System.out.println(result);
+    }
 }
